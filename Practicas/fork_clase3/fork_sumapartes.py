8832348ae7431717f888fd5b94e49d1a3d540639
agustinaguero97 <agustin1997aguero@gmail.com>
2022-04-04 20:03:24 -0300

    sumarpartes hecho con fork

---
+++ Practicas/fork_clase3/fork_sumapartes.py
@@ -0,0 +1,89 @@
+"""
+    Escribir un programa en Python que reciba los siguientes argumentos por línea de comandos:
+
+-n <numero>
+-h
+-v
+El programa debe generar <numero> procesos hijos, 
+y cada proceso calculará la suma de todos los números enteros pares entre 0 y su número de PID.
+
+El programa deberá mostrar por pantalla:
+
+PID – PPID : <suma_pares>
+El proceso padre debe esperar a que todos sus hijos terminen.
+
+La opción -h mostrará ayuda de uso, y la opción -v habilitará el modo verboso de la aplicación. 
+El modo verboso debe mostrar, además de la suma, un mensaje al inicio y al final de la ejecución de cada proceso hijo, 
+que indique su inicio y fin.
+
+Ejemplos 1:
+
+./sumapares.py -n 2
+32803 – 4658: 269009202
+32800 – 4658: 268943600
+
+Ejemplos 2:
+
+./sumapares.py -n 2 -v
+Starting process 32800
+Starting process 32803
+Ending process 32803
+32803 – 4658: 269009202
+Ending process 32800
+32800 – 4658: 268943600
+"""
+
+import os,argparse
+
+def ChildProcess(verboso):
+
+    child =os.getpid()
+
+    if verboso == 1:
+        print(f"starting process {child}")
+        suma = sumador()
+        print(f"PID {child} - PPID {os.getppid()}: {suma}")
+        print(f"ending process: {child}")
+    else:
+        suma = sumador()
+        print(f"PID {child} - PPID {os.getppid()}: {suma}")
+            
+
+def sumador():
+    suma = 0
+    for par in range(0,int(os.getpid())+1,2):
+        suma = suma + par
+    return suma
+
+# Fork and create a child process
+def creation(verboso):
+    #crea los hijos
+    retVal = os.fork()
+
+
+
+    # Separate logic for parent and child
+
+    if retVal == 0:
+        #inicio la rutina del proceso hijo
+        ChildProcess(verboso)
+        #cierro el proceso hijo, sino tendre un hijo que haga mas hijos
+        os._exit(0)
+    else:
+
+        os.wait()
+        
+def main():
+    #Defino el parseo de argumentos
+    parser = argparse.ArgumentParser(usage="\nsumapartes.py [-h HELP] [-n NUMERO] [-v VERBOSO]")
+    parser.add_argument('-n', '--numero', metavar='NUMERO', type=int, default=1, help="Cantidad de procesos hijos a crear")
+    parser.add_argument('-v', '--verboso', metavar='VERBOSO', type=int, default=0 , help="Activa modo verboso colocando un 1")
+    args = parser.parse_args()
+    numero = args.numero
+    verboso = args.verboso
+    
+    #crea la cantidad de hijos solicitados
+    for x in range(1,numero+1):
+        creation(verboso)
+if __name__ == "__main__":
+    main()
